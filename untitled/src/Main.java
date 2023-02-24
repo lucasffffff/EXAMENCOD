cb25dffdbd89de0189a29d141cd1227e389ee104
lucasf_ <lfeliualvarez@danielcastelao.org>
2023-02-24 10:15:33 +0100

    Añado la operación que calcula cualquier tipo de indice y mejoro javadoc y un poco el código

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,18 +1,34 @@
+/**
+ * Clase principal que ejecuta una operación aritmética utilizando la clase Operaciones.
+ */
 public class Main {
+
     /**
-     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN)
-     * @param num1 --> primer numero de la operación
-     * @param num2 --> segundo numero de la operación (divisor en caso de division)
-     * @return resultado o null en caso de fallo.
+     * Realiza una operación aritmética especificada y muestra el resultado por consola.
+     * @param device el tipo de operación a realizar. Los valores permitidos son Operaciones.SUMA,
+     *               Operaciones.RESTA, Operaciones.MULTIPLICACION y Operaciones.DIVISION.
+     * @param num1 el primer número de la operación.
+     * @param num2 el segundo número de la operación, en el caso de la división representa el divisor.
+     * @return el resultado de la operación o null si ocurrió un error.
+     */
+    public static Float realizarOperacion(int device, float num1, float num2) {
+        Operaciones operaciones = new Operaciones();
+        return operaciones.calculadora(device, num1, num2);
+    }
+
+    /**
+     * Método principal que ejecuta una operación de suma y muestra el resultado por consola.
+     * @param args argumentos de línea de comandos (no se utilizan).
      */
     public static void main(String[] args) {
-
-        Operaciones metodos = new Operaciones();
         float num1 = 20;
         float num2 = 10;
-        Float resultado = metodos.calculadora(Operaciones.SUMA, num1, num2);
+        Float resultado = realizarOperacion(Operaciones.SUMA, num1, num2);
 
-        System.out.println("El resultado de la suma es: " + resultado);
-
+        if (resultado != null) {
+            System.out.println("El resultado de la suma es: " + resultado);
+        } else {
+            System.out.println("Ocurrió un error al realizar la operación.");
+        }
     }
 }
--- untitled/src/Operaciones.java
+++ untitled/src/Operaciones.java
@@ -1,3 +1,9 @@
+/**
+
+ Esta clase define varias operaciones matemáticas, como la suma, la resta, la multiplicación,
+
+ la división, la raíz cuadrada, la raíz cúbica y la raíz de cualquier índice.
+ */
 public class Operaciones {
 
     private static Float resultado = null;
@@ -5,56 +11,85 @@ public class Operaciones {
     static final int RESTA = 2;
     static final int MULTIPLICACION = 3;
     static final int DIVISION = 4;
-    static final int RAIZ_CUADRADA = 5;
+    static final int RAIZ_CUADRADA = 10;
     static final int RAIZ_CUBICA = 6;
+    static final int RAIZ = 5;
 
+/**
 
-    public static Float calculadora(int device, float num1, float num2){
+ Calcula el resultado de una operación matemática con dos números.
+ @param device el tipo de operación a realizar (suma, resta, multiplicación, división,raíz cuadrada o raíz cúbica)
+ @param num1 el primer número de la operación
+ @param num2 el segundo número de la operación (divisor en caso de división o índice en caso de raíz)
+ @return el resultado de la operación, o null en caso de fallo.
+ */
+public static Float calculadora(int device, float num1, float num2) {
+    switch (device) {
+        case SUMA:
+            try {
+                resultado = num1 + num2;
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+            break;
+        case RESTA:
+            try {
+                resultado = num1 - num2;
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+            break;
+        case MULTIPLICACION:
+            try {
+                resultado = num1 * num2;
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+            break;
+        case DIVISION:
+            try {
+                resultado = num1 / num2;
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+            break;
+        case RAIZ_CUADRADA:
+            try {
+                resultado = (float) Math.sqrt(num1);
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+            break;
+        case RAIZ_CUBICA:
+            try {
+                resultado = (float) Math.cbrt(num1);
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+            break;
+        case RAIZ:
+            resultado = raiz(num1, num2);
+            break;
+    }
+    return resultado;
+}
+    /**
 
-        switch (device){
-            case SUMA:
-                try{
-                    resultado = num1 + num2;
-                }catch (Exception e){
-                    System.out.println("Error");
-                }
-                break;
-            case RESTA:
-                try{
-                    resultado = num1 - num2;
-                }catch (Exception e){
-                    System.out.println("Error");
-                }
-                break;
-            case MULTIPLICACION:
-                try{
-                    resultado = num1*num2;
-                }catch (Exception e){
-                    System.out.println("Error");
-                }
-                break;
-            case DIVISION:
-                try{
-                    resultado = num1/num2;
-                }catch (Exception e){
-                    System.out.println("Error");
-                }
-                break;
-            case RAIZ_CUADRADA:
-                try{
-                    resultado = (float) Math.sqrt(num1);
-                }catch (Exception e){
-                    System.out.println("Error");
-                }
-                break;
-            case RAIZ_CUBICA:
-                try{
-                    resultado = (float) Math.cbrt(num1);
-                }catch (Exception e){
-                    System.out.println("Error");
-                }
-                break;
+     Calcula la raíz de cualquier índice de un número.
+     @param num1 el radicando
+     @param num2 el índice de la raíz
+     @return el resultado de la raíz, o null en caso de fallo.
+     */
+    public static Float raiz(float num1, float num2) {
+        try {
+            resultado = (float) Math.pow(num1, 1 / num2);
+        } catch (Exception e) {
+            System.out.println("Error");
         }
         return resultado;
     }
 }
+
+
+
+
